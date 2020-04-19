eea2de6b109d23f808d39da3faf08f8e7675b44d
zhuwei <zhuwei_leo@163.com>
2020-04-19 20:47:03 +0800

    完成基本功能

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import classNamesBind from 'classnames/bind';
+import style from './index.scss';
+
+export default function <%= name %>() {
+    return (
+      <div></div>
+    )
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,82 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+
+const { window, workspace, Uri } = vscode;
+
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "create-component" is now active!');
+	console.log(context);
+	
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let disposable = vscode.commands.registerCommand('extension.showFileDir', async (uri) => {
+		
+		const watcher = fs.watch(uri.fsPath,function (e: any, name:any) {
+			console.log(uri.fsPath, name);
+			
+			
+			const dir = path.resolve(uri.fsPath,name);
+
+			const fileMap = [
+				{ id: '1', path: '../assets/index.js', name: 'index.js' },
+				{ id: '2', path: '../assets/index.scss', name: 'index.scss' }
+			];
+
+			fileMap.forEach((file) => {
+				const fileContent = fs.readFileSync(path.join(__dirname, file.path), { encoding: 'utf8' });
+				let result = fileContent;
+				if(file.id === '1') {
+					const template = ejs.compile(fileContent)
+					result = template({name})
+				}
+				
+				fs.writeFileSync(path.resolve(dir, file.name), result);
+			});
+			watcher.close();
+		})
+		
+		const res = await vscode.commands.executeCommand('explorer.newFolder');
+		// console.log(uri);
+		// console.log(
+		// 	workspace.workspaceFile,
+		// 	workspace.workspaceFolders,
+		// 	);
+		
+
+		
+
+		// const name = await window.showInputBox({placeHolder: '请输入组件名'})
+		// if(!name) return;
+		
+		// const dir = path.resolve(uri.fsPath,name);
+		// const myUri = Uri.file(dir);
+		// fs.mkdirSync(dir);
+
+		// const fileMap = [
+		// 	{ id: '1', path: '../assets/index.js', name: 'index.js' },
+		// 	{ id: '2', path: '../assets/index.scss', name: 'index.scss' }
+		// ];
+
+		// fileMap.forEach((file) => {
+		// 	const fileContent = fs.readFileSync(path.join(__dirname, file.path), { encoding: 'utf8' });
+		// 	let result = fileContent;
+		// 	if(file.id === '1') {
+		// 		const template = ejs.compile(fileContent)
+		// 		result = template({name})
+		// 	}
+			
+		// 	fs.writeFileSync(path.resolve(dir, file.name), result);
+		// });
+
+		
 	});
 
 	context.subscriptions.push(disposable);
