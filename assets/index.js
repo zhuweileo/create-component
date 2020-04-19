427d9dd3e3cc827589625b5ca6e6448b57b08333
zhuwei <zhuwei_leo@163.com>
2020-04-19 22:40:03 +0800

    修改模板

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import classNamesBind from 'classnames/bind';
 import style from './index.scss';
 
+const cx = classNamesBind.bind(style);
+
 export default function <%= name %>() {
     return (
       <div></div>
