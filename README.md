fa856ec72d82d2524949ac979c58c1b7ad64be23
zhuwei <zhuwei_leo@163.com>
2020-05-04 00:13:28 +0800

    完善组件

--- README.md
+++ README.md
@@ -1,11 +1,78 @@
-## 插件开发官方文档
-https://code.visualstudio.com/api
+# Create Component
+### [github](https://github.com/zhuweileo/create-component)  
+在开发中，我们经常需要初始化一个组件，组件的初始化，有时候会包含多个文件，例如：一个react组件会包含一个`jsx`文件和一个`css`样式文件。
 
-## 脚手架
-官方：https://github.com/Microsoft/vscode-generator-code
+### 以往，我们初始化该组件就需要执行如下操作：
+1. 新建一个文件夹；
+2. 创建jsx文件，复制一坨代码（或者用snippets生成）；
+3. 创建css文件。
 
-## 命令
-vsce package // 打包
+### 该插件将以上步骤简化为：
+1. 创建文件夹；
+2. 选择预定义的组件模板。
 
-## 参考
-[vscode参见开发全攻略](https://www.cnblogs.com/liuxianan/p/vscode-plugin-overview.html)
+或者：
+1. 创建文件夹，并直接生成用户定义的默认组件模板。
+
+
+# 配置
+
+```json
+    "createComponent.userTpl": [
+        {
+            "name": "React模板",
+            "default": true,
+            "paths": [
+                "F:\\文件夹名称\\index.tsx",
+                "F:\\文件夹名称\\index.scss"
+            ]
+        },
+        {
+            "name": "Vue模板",
+            "paths": [
+                "F:\\文件夹名称\\index.ts",
+                "F:\\文件夹名称\\index.scss",
+                "F:\\文件夹名称\\index.vue"
+            ]
+        }
+    ]
+```
+每个用户自定义模板配置包含以下属性：
+```ts
+interface tplItem {
+	name: string; // 模板名称，模板的唯一标识
+	default?: boolean; // 是否设置为默认模板，如果为true，执行New Default Component命令时会直接使用该模板
+	paths: string[]; // 组件包含的所有文件的绝对路径
+}
+```
+
+# 使用方法
+1. 使用前，请先配置好自己的模板文件的存放位置。
+
+2. 在文件夹内点击鼠标右键时，菜单内会包含 `New Component` 和 `New Default Component` 项，这两个选项类似于新建文件夹命令，不同之处在于，会在文件夹内生成用户选择的模板文件。
+
+3. 模板文件支持 ejs 模板语法
+ejs 文档参考 https://ejs.bootcss.com/
+目前支持2个预定义变量
+name: 用户新建文件夹的名字
+date: 当前时间对象
+
+```js
+/**
+ * 创建日期 <%= date %>
+ */
+
+import React, { useEffect, useState } from 'react';
+import classNamesBind from 'classnames/bind';
+import style from './index.scss';
+
+const cx = classNamesBind.bind(style);
+
+export default function <%= name %>() {
+    return (
+      <div></div>
+    )
+}
+```
+
+
--- src/extension.ts
+++ src/extension.ts
@@ -9,51 +9,72 @@ const { window, workspace, Uri } = vscode;
 
 interface tplItem {
 	name: string;
+	default?: boolean;
 	paths: string[];
+}
+
+function createFiles(fileMap: string[], componentName: string, dir: string) {
+	fileMap.forEach((filePath) => {
+		const fileName = path.basename(filePath);
+		fs.readFile(path.resolve(filePath), function(err: any, data: Buffer) {
+			if (!err) {
+				const template = ejs.compile(data.toString('utf8'));
+				const result = template({ name: componentName, date: new Date() });
+				fs.writeFileSync(path.resolve(dir, fileName), result);
+			} else {
+				vscode.window.showErrorMessage(`读取${filePath}失败，${err.toString()}`);
+				throw err;
+			}
+		});
+	});
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const userTplList: tplItem[] = vscode.workspace.getConfiguration().get('createComponent.useTpl') as tplItem[];
+	const userTplList: tplItem[] = vscode.workspace.getConfiguration().get('createComponent.userTpl') as tplItem[];
+
 	let disposable = vscode.commands.registerCommand('extension.createComponent', async (uri) => {
 		const watcher = fs.watch(uri.fsPath, async function(e: any, name: any) {
 			watcher.close();
-			
+
+			const dir = path.resolve(uri.fsPath, name);
+			let fileMap: string[] = [];
+			const tplName = await vscode.window.showQuickPick(userTplList.map((item) => item.name));
+			fileMap = userTplList.filter((item) => item.name === tplName)[0].paths;
+			createFiles(fileMap, name, dir);
+		});
+
+		vscode.commands.executeCommand('explorer.newFolder');
+	});
+
+	// 创建默认组件
+	let disposable2 = vscode.commands.registerCommand('extension.createDefaultComponent', async (uri) => {
+		const watcher = fs.watch(uri.fsPath, async function(e: any, name: any) {
+			watcher.close();
+
 			const dir = path.resolve(uri.fsPath, name);
 
-			// const fileMap = [
-			// 	{ id: '1', path: '../assets/index.tsx', name: 'index.tsx' },
-			// 	{ id: '2', path: '../assets/index.scss', name: 'index.scss' }
-			// ];
-			const tplName = await vscode.window.showQuickPick(userTplList.map(item => item.name));
-			
-			console.log(tplName);
+			const userDefault = userTplList.filter((item) => item.default);
+			const hasUserDefault = userDefault.length;
 
-			const fileMap: string[] = userTplList.filter(item => item.name === tplName)[0].paths;
+			let fileMap: string[] = [];
+			if (hasUserDefault) {
+				// 用户指定了默认项
+				fileMap = userDefault[0].paths;
+			} else {
+				const tplName = await vscode.window.showQuickPick(userTplList.map((item) => item.name));
+				fileMap = userTplList.filter((item) => item.name === tplName)[0].paths;
+			}
 
-			fileMap.forEach((filePath) => {
-				const fileName = path.basename(filePath);
-
-				
-				fs.readFile(path.resolve(filePath), function(err: any, data: Buffer) {
-
-					if (!err) {
-						const template = ejs.compile(data.toString('utf8'));
-						const result = template({ name, date: new Date() });
-						fs.writeFileSync(path.resolve(dir, fileName), result);
-					} else {
-						vscode.window.showErrorMessage(`读取${filePath}失败，${err.toString()}`);
-						throw err;
-					}
-				});
-			});
+			createFiles(fileMap, name, dir);
 		});
 
 		vscode.commands.executeCommand('explorer.newFolder');
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
